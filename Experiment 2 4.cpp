9d37d22bf6321155a78f74a6a311d96a0dfb5122
Gavin Lin <47804418+LinKG2699@users.noreply.github.com>
2019-05-13 21:17:52 +0800

    Add files via upload

---
+++ Experiment 2 4.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <conio.h>
+#include <cstdlib>
+using namespace std;
+
+int main()
+{
+	int counter;
+	cout << "counting...\n";
+	for (counter=1; counter <=10; counter++)
+	{	
+		cout << counter << ", ";
+	
+	}
+	int count;
+	for (count=12 ; count<=30 ; count++)
+	{		
+		cout << count << ", ";
+		
+		if(counter==13) continue;
+		cout << "14, ";
+		if(counter==15) continue;
+		cout << "16, ";
+		if (counter==17) continue;
+		cout << "18, ";
+		if (counter==19) continue;
+		cout << "20, ";
+		if (counter==21) continue;
+			cout << "22, ";
+		if (counter==23) continue;
+			cout << "24, ";
+		if (counter==25) continue;
+			cout << "26, ";
+		if (counter==27) continue;
+			cout << "28, ";	
+		if (counter==29) continue;
+			cout << "30";
+
+		break;
+}
+	_getch();
+	return 0;
+
+}
